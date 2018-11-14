e0f65dcbf1519c7a6405d31d6c2f52351f0f3853
HuseyinOrkun <huseyinorkunelmas@gmail.com>
2018-11-14 09:06:59 +0300

    added playerSelection screen

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,15 +2,26 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("mainMenu.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        FXMLLoader psloader = new FXMLLoader(getClass().getResource("playerSelection.fxml"));
+        Parent playerSelect  = psloader.load();
+        playerSelectionController pscontroller = psloader.getController();
+
+        // suppliying List of player names here, in the future it will come from another place
+        String[] pslist = new String[]{"sena", "Hüseyin"};
+
+        // Set the playerList & setSpinner values to playerlist
+        pscontroller.setPlayerList(pslist);
+        pscontroller.setSpinner();
+
+        //primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(playerSelect, 700, 500));
         primaryStage.show();
     }
 
