2a49a52422030ca667ba96b9037473646a3237a5
zson <zeynep.sonkaya@ug.bilkent.edu.tr>
2018-11-17 17:33:51 +0300

    add gamecontroller

---
+++ src/main/java/GameController.java
@@ -0,0 +1,12 @@
+public class GameController {
+
+private GameBoard board;
+private boolean isFull(){}
+private void placePentomino(){}
+private void movePentomino (){}
+private void clashCheck(){}
+private void drawBorder(){}
+private void colorClashingCells(){}
+private void startGame(){}
+private void pauseGame(){}
+}
