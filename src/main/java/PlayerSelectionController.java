82a5f646d758f266a9017cf72d7dfb7e47bb5b36
HuseyinOrkun <huseyinorkunelmas@gmail.com>
2018-11-19 00:22:38 +0300

    add filemanager, read data from json, maven

--- src/main/java/PlayerSelectionController.java
+++ src/main/java/PlayerSelectionController.java
@@ -6,29 +6,36 @@ import javafx.scene.control.Label;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.SpinnerValueFactory;
 import javafx.scene.control.TextField;
+import kataminoBackButton.KataminoBackButton;
 import kataminoButton.KataminoButton;
+import kataminoPlayerAddButton.KataminoPlayerAddButton;
+
+import java.io.File;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 public class PlayerSelectionController implements Initializable {
 
     @FXML private Spinner spinner;
     @FXML private KataminoButton continueButton;
-    @FXML private KataminoButton createPlayerButton;
+    @FXML private KataminoPlayerAddButton createPlayerButton;
     @FXML private TextField playerNameField;
     @FXML private Label errorLabel;
-    private String[] playerList;
-    private ObservableList<String> players = FXCollections.observableArrayList();
+    @FXML private KataminoBackButton backButton;
+    private ObservableList<String> players;
     private SpinnerValueFactory<String> valueFactory;
+    private FileManager fm;
+    private ArrayList<String> savedPlayers;
     public PlayerSelectionController() {
-    
+
     }
 
     public void initialize(URL location, ResourceBundle resources) {
+        fm = new FileManager();
         continueButton.setButtonName("Continue");
-        createPlayerButton.setButtonName("Create");
-    }
-    public void setSpinner(){
-        players.addAll(playerList);
+        players = FXCollections.observableArrayList();
+        savedPlayers = fm.loadPlayerNames();
+        players.addAll(savedPlayers);
         valueFactory = //
                 new SpinnerValueFactory.ListSpinnerValueFactory<String>(players);
         valueFactory.setValue(players.get(0));
@@ -38,29 +45,33 @@ public class PlayerSelectionController implements Initializable {
     @FXML
     public void continueButtonClicked(){
         String selectedPlayerName = (String) spinner.getValue();
-        System.out.print(selectedPlayerName);
-        // get the spinner value
-        // continue to the next screen
+        System.out.print("Selected Player: "+selectedPlayerName);
+        if(savedPlayers.contains(selectedPlayerName)){
+            Player selectedPlayer = fm.loadPlayer(selectedPlayerName);
+        }
+        else{
+            Player newPlayer = new Player(selectedPlayerName);
+        }
+
+        // Todo: setPlayer of something Maybe even savePlayer here with initial values
     }
 
     @FXML
     public void createButtonClicked(){
         String newPlayerName = playerNameField.getText();
         errorLabel.setText("");
-        // Todo: needs logic like should not contain " " etc
+        if(!newPlayerName.endsWith(" ")){
         if (!players.contains(newPlayerName)) {
-            // Todo: Create a player with name newplayername
             System.out.println(newPlayerName);
             players.add(newPlayerName);
             valueFactory.setValue(newPlayerName);
-            spinner.setValueFactory(valueFactory); // We can change these if not liked
+            spinner.setValueFactory(valueFactory);
         }
         else{
             errorLabel.setText("Player Name Already Taken!!");
         }
-
-    }
-    public void setPlayerList(String[] playerList) {
-        this.playerList = playerList;
+        }
+        // TODO: can add more
+        errorLabel.setText("Player Name Can Not End With Empty Space!!");
     }
 }
