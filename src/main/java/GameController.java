1eed9633a11ee443041c12015e6e07001cc68c2e
Utgoer <utgoerweb@gmail.com>
2018-12-19 21:08:58 +0300

    fix isFull condition2

--- src/main/java/GameController.java
+++ src/main/java/GameController.java
@@ -65,7 +65,7 @@ public abstract class GameController implements Initializable {
         ObservableList<Node> cells = gameTilePane.getChildren();
         for (int i = 0; i < cells.size(); i++) {
             KataminoDragCell currentPentomino = (KataminoDragCell) cells.get(i);
-            if ((currentPentomino.getPentominoInstanceID() == 0)) {
+            if ((currentPentomino.isOnBoard() && (currentPentomino.getPentominoInstanceID() == -1)) || (currentPentomino.getPentominoInstanceID() == 0)) {
                 return false;
             }
         }
