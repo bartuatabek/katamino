1536c577026bd9a44ff6523396d2c7d0994ae627
Bartu Atabek <bartu.atabek@icloud.com>
2018-12-21 17:13:04 +0300

    Level transitions optimized.

--- src/main/java/GameBoard.java
+++ src/main/java/GameBoard.java
@@ -11,28 +11,28 @@ public class GameBoard {
 
     private Level currentLevel;
 
-    Integer[][]board;
+    private ArrayList<Color> colorList = new ArrayList<Color>(){{
+        add(Color.web("FF3B30"));
+        add(Color.web("FF9500"));
+        add(Color.web("FFCC00"));
+        add(Color.web("4CD964"));
+        add(Color.web("5AFAFA"));
+        add(Color.web("007AFF"));
+        add(Color.web("5856D6"));
+        add(Color.web("FF2D55"));
+        add(Color.web("8B572A"));
+        add(Color.web("B8E986"));
+        add(Color.web("BD10E0"));
+        add(Color.web("FF5700"));
+    }};
 
-    private ArrayList<Color> colorList ;
+    Integer[][]board;
 
     public GameBoard(int levelNo) {
         currentLevel = new Level(levelNo);
         board = currentLevel.getBoard();
         grid = new KataminoDragCell[board.length][board[0].length];
-        colorList = new ArrayList<Color>(){{
-            add(Color.web("FF3B30"));
-            add(Color.web("FF9500"));
-            add(Color.web("FFCC00"));
-            add(Color.web("4CD964"));
-            add(Color.web("5AFAFA"));
-            add(Color.web("007AFF"));
-            add(Color.web("5856D6"));
-            add(Color.web("FF2D55"));
-            add(Color.web("8B572A"));
-            add(Color.web("B8E986"));
-            add(Color.web("BD10E0"));
-            add(Color.web("FF5700"));
-        }};
+
         try {
             loadLevel();
         } catch (FileNotFoundException f) {
@@ -45,7 +45,12 @@ public class GameBoard {
     public void loadLevel() throws IOException {
         for (int i= 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
+
+//                (KataminoDragCell) board.getChildren().get((i*22)+j);
                 KataminoDragCell currentCell = new KataminoDragCell();
+
+
+
                 Color cellColor = ((board[i][j] % 12) >= 0 && (board[i][j] != 0)) ? colorList.get(board[i][j] % 12) : Color.web("#262626");
                 currentCell.customizeCell(board[i][j], board[i][j] == 0, cellColor);
                 if (currentCell.getPentominoInstanceID() == 0) {
@@ -62,5 +67,18 @@ public class GameBoard {
 
     public void setGrid(KataminoDragCell[][] grid) {
         this.grid = grid;
+    }
+
+    public void updateGameBoard(int levelNo) {
+        currentLevel = new Level(levelNo);
+        board = currentLevel.getBoard();
+
+        try {
+            loadLevel();
+        } catch (FileNotFoundException f) {
+            System.out.println("IOException");
+        } catch (IOException ex) {
+            System.out.println("General exception");
+        }
     }
 }
