93362ac2a039a0b432e972e9b5d029a24363bf95
Bartu Atabek <bartu.atabek@icloud.com>
2018-12-25 01:29:50 +0300

    kataminoBoardButton custom icon added.

--- lib/components/src/kataminoBoardButton/KataminoBoardButton.java
+++ lib/components/src/kataminoBoardButton/KataminoBoardButton.java
@@ -17,6 +17,9 @@ public class KataminoBoardButton extends Pane {
     @FXML
     ImageView boardImage3;
 
+    @FXML
+    ImageView boardImage4;
+
     private int id;
 
     public KataminoBoardButton() throws IOException {
@@ -32,21 +35,25 @@ public class KataminoBoardButton extends Pane {
             boardImage1.setVisible(true);
             boardImage2.setVisible(false);
             boardImage3.setVisible(false);
+            boardImage4.setVisible(false);
         } else if (value == 1) {
             id = 1;
             boardImage1.setVisible(false);
             boardImage2.setVisible(true);
             boardImage3.setVisible(false);
+            boardImage4.setVisible(false);
         } else if (value == 2) {
             id = 2;
             boardImage1.setVisible(false);
             boardImage2.setVisible(false);
             boardImage3.setVisible(true);
+            boardImage4.setVisible(false);
         } else {
             id = value;
-            boardImage1.setVisible(true);
+            boardImage1.setVisible(false);
             boardImage2.setVisible(false);
             boardImage3.setVisible(false);
+            boardImage4.setVisible(true);
         }
     }
 
